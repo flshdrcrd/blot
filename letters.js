c16e2616ef6adb728cffcdc4047184a93cb05a12
flshdrcrd <74014986+flshdrcrd@users.noreply.github.com>
2024-07-17 12:09:24 +0200

    Update letters.js

--- letters.js
+++ letters.js
@@ -1,54 +1,150 @@
+const START_X = 20
+const START_Y = 250
+const LETTER_WIDTH = 12
+const LETTER_HEIGHT = 22
+const LETTER_GAP = 2
+const TEXT = "A AAA A"
+
+// - - - - - - - - - - 
+
 const width = 200;
 const height = 300;
-
 setDocDimensions(width, height);
 
-function drawLetter(letter, x, y, size) {
+function drawLetter(letter, x, y, w, h) {
+  let t = y + h;
+  let b = y;
+  let l = x;
+  let r = x + w;
+
+  let bl = [x, y];
+  let br = [x + w, y];
+  let tl = [x, y + h];
+  let tr = [x + w, y + h];
+
   if (letter == 'A') {
-    let w = x + size * 0.5;
-    let h = y + size;
-
-    let polyline_0 = [
-      [x, y],
-      [w / 2, h]
+    let line_0 = [
+      bl,
+      [l + w / 2, t]
     ];
-    let polyline_1 = [
-      [w / 2, h],
-      [w, y]
+    let line_1 = [
+      [l + w / 2, t],
+      br
     ];
-    let polyline_2 = [bt.bounds([polyline_0]).cc, bt.bounds([polyline_1]).cc];
+    let line_2 = [bt.bounds([line_0]).cc, bt.bounds([line_1]).cc];
 
-    let polylines = [polyline_0, polyline_1, polyline_2];
-    drawLines(polylines);
+    let lines = [line_0, line_1, line_2];
+    drawLines(lines);
 
   } else if (letter == 'B') {
-    let w = x + size * 0.5;
-    let h = y + size;
-
-    let polyline_0 = [
+    let line_0 = [
       [x, y],
       [x, h]
     ];
-    let polyline_1 = [
+    let line_1 = [
       [x, h],
       [w, h],
-      [w, h /2],
+      [w, h / 2],
       [x, h / 2]
     ];
-    let polyline_2 = [
-      [x, h/2],
-      [w, h/2],
+    let line_2 = [
+      [x, h / 2],
+      [w, h / 2],
       [w, y],
       [x, y]
     ];
-    let polyline_3 = bt.nurbs(polyline_1)
-    let polyline_4 = bt.nurbs(polyline_2)
+    let line_3 = bt.nurbs(line_1);
+    let line_4 = bt.nurbs(line_2);
 
+    let lines = [line_0, line_3, line_4];
+    drawLines(lines);
+  } else if (letter == 'C') {
+    let line_0 = [
+      [w, h],
+      [x, h],
+      [x, y],
+      [w, y]
+    ];
 
-    let polylines = [polyline_0, polyline_3, polyline_4];
-    drawLines(polylines);
+    let line_1 = bt.nurbs(line_0);
+
+    let lines = [line_1];
+    drawLines(lines);
+  } else if (letter == 'D') {
+    let line_0 = [
+      [x, y],
+      [x, h]
+    ];
+    let line_1 = [
+      [x, h],
+      [w, h],
+      [w, y],
+      [x, y]
+    ];
+    let line_2 = bt.nurbs(line_1);
+
+    let lines = [line_0, line_2];
+    drawLines(lines);
+  } else if (letter == 'E') {
+    let line_0 = [
+      [x, y],
+      [x, h]
+    ];
+    let line_1 = [
+      [x, h],
+      [w, h]
+    ];
+    let line_2 = [
+      [x, h / 2],
+      [w, h / 2]
+    ];
+    let line_3 = [
+      [x, y],
+      [w, y]
+    ];
+
+    let lines = [line_0, line_1, line_2, line_3];
+    drawLines(lines);
+  } else if (letter == 'F') {
+    let line_0 = [
+      [x, y],
+      [x, h]
+    ];
+    let line_1 = [
+      [x, h],
+      [w, h]
+    ];
+    let line_2 = [
+      [x, h / 2],
+      [w, h / 2]
+    ];
+    let line_3 = [
+      [x, y],
+      [w, y]
+    ];
+
+    let lines = [line_0, line_1, line_2];
+    drawLines(lines);
+  } else if (letter == 'G') {
+    let line_0 = [
+      [w, h],
+      [x, h],
+      [x, y],
+      [w, y]
+    ];
+    let line_1 = [
+      [w, y],
+      [w, h / 2],
+      [x + (w - x) / 2, h / 2]
+    ];
+    let line_2 = bt.nurbs(line_0);
+
+    let lines = [line_1, line_2];
+    drawLines(lines);
   }
 }
 
-drawLetter('A', 0, 0, 90);
-drawLetter('B', 50, 0, 90);
+let splitText = TEXT.split("")
+for (let i = 0; i < splitText.length; i++) {
+  drawLetter(splitText[i], START_X + i * (LETTER_WIDTH + LETTER_GAP), START_Y, LETTER_WIDTH, LETTER_HEIGHT);
+}
